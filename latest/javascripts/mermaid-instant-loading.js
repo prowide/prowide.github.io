cc8f18fea5c0e76759e547b8c9efaa7bbe740243
zubri <sebastian@prowidesoftware.com>
2026-03-06 18:11:58 -0300

    Publish SRU2025-10 documentation

--- latest/javascripts/mermaid-instant-loading.js
+++
@@ -1,9 +0,0 @@
-// Fix for Mermaid diagrams with instant loading
-// Reinitialize Mermaid after instant navigation
-document$.subscribe(function() {
-  // Wait for mermaid to be loaded
-  if (typeof mermaid !== 'undefined') {
-    // Reinitialize all mermaid diagrams on the page
-    mermaid.init(undefined, document.querySelectorAll('.mermaid'));
-  }
-});
